257f82b2308ca0d939213262f3e8a79b565d7234
camiloschier <peronacamilo@gmail.com>
2019-07-21 21:29:32 -0300

    v0.20

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import DotLoader from 'react-spinners/DotLoader';
+import Zoom from 'react-reveal/Zoom';
 import Tarjeta from './Tarjetas/Tarjeta'
 import axios from 'axios';
+
+const override = {
+    display: 'block',
+    margin: '0 auto',
+    borderColor: 'red',
+  };
+
+
 
 class Home extends Component {
   constructor() {
     super()
     this.state = {
       posteos: [],
-      user: ''
+      user: '',
+      loading: false
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -23,9 +34,10 @@ class Home extends Component {
   }
 
  getDataFromUser = async(user) => {
+   this.setState({posteos: [],loading:true})
    await axios.get('https://api.pushshift.io/reddit/search/submission/?author='+user)
   .then(response => {
-    this.setState({posteos: response.data.data})
+    this.setState({posteos: response.data.data, loading:false})
 
   })
   .catch(error => {
@@ -45,11 +57,21 @@ class Home extends Component {
             </div>
             <input type="text" name="user" value={this.state.user} onChange={this.handleChange} className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
               <div className="input-group-append">
-            <span className="input-group-text" id="basic-addon2"><a className="btn btn-primary" onClick={()=>this.getDataFromUser("ThePieous")} >BUSCAR</a></span>
+            <span className="input-group-text" id="basic-addon2"><button className="btn btn-primary" onClick={()=>this.getDataFromUser(this.state.user)} >BUSCAR</button></span>
           </div>
+        </div>
+        <div className='sweet-loading'>
+          <DotLoader
+            css={override}
+            sizeUnit={"px"}
+            size={150}
+            color={'#123abc'}
+            loading={this.state.loading}
+          />
         </div>
           <div className="row">
               {this.state.posteos.map( posteo =>(
+                <Zoom>
                 <div className="col-sm-4">
                   <Tarjeta
                     title={posteo.title}
@@ -57,6 +79,7 @@ class Home extends Component {
                     link={posteo.full_link}
                   />
                 </div>
+              </Zoom>
               ))}
           </div>
 
